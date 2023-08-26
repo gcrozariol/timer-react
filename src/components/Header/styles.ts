039597656a4e98ea04f4d77600e55b4debafc9a7
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-08-26 14:49:15 -0300

    Add header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      width: 3rem;
+      height: 3rem;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      transition: 0.1s;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
