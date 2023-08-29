c85d15efc014b15ef4607befae718b997bd011c2
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-08-28 22:53:35 -0300

    Add immer js + implement it to reducer for better code readability

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from 'immer'
 import { ActionTypes } from './actions'
 
 export interface Cycle {
@@ -28,40 +29,46 @@ export function cyclesReducer(
   const { type, payload } = action
 
   switch (type) {
-    case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...cycles, payload.newCycle],
-        activeCycleId: payload.newCycle.id,
-      }
+    case ActionTypes.ADD_NEW_CYCLE: {
+      return produce(state, (draft) => {
+        draft.cycles.push(payload.newCycle)
+        draft.activeCycleId = payload.newCycle.id
+      })
+    }
 
-    case ActionTypes.INTERRUPT_CYCLE:
+    case ActionTypes.INTERRUPT_CYCLE: {
       document.title = 'Ignite Timer'
 
-      return {
-        ...state,
-        cycles: cycles.map((cycle) => {
-          if (cycle.id === activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
+      const currentCycleIndex = cycles.findIndex(
+        (cycle) => cycle.id === activeCycleId,
+      )
+
+      if (currentCycleIndex < 0) {
+        return state
       }
 
-    case ActionTypes.MARK_CYCLE_AS_COMPLETED:
-      return {
-        ...state,
-        cycles: cycles.map((cycle) => {
-          if (cycle.id === activeCycleId) {
-            return { ...cycle, finishedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
+      return produce(state, (draft) => {
+        draft.cycles[currentCycleIndex].interruptedDate = new Date()
+        draft.activeCycleId = null
+      })
+    }
+
+    case ActionTypes.MARK_CYCLE_AS_COMPLETED: {
+      document.title = 'Ignite Timer'
+
+      const currentCycleIndex = cycles.findIndex(
+        (cycle) => cycle.id === activeCycleId,
+      )
+
+      if (currentCycleIndex < 0) {
+        return state
       }
+
+      return produce(state, (draft) => {
+        draft.cycles[currentCycleIndex].finishedDate = new Date()
+        draft.activeCycleId = null
+      })
+    }
 
     default:
       return state
